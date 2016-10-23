eff2f03ebac5c965f572a34e75723ed8ed283367
ashok.kumar <ashok.kumar@dunstconsulting.in>
2016-10-23 20:19:29 +0530

    Initial commit Stage 1 commit:
    Good example for understanding integrating the firebase analytics.
    It explains all the basic things needed for firebase analytics integration.

---
+++ app/src/main/java/com/ashokslsk/firebaseanalytics/MainActivity.java
@@ -0,0 +1,38 @@
+package com.ashokslsk.firebaseanalytics;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnlytics;
+    private Button mFireclick;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFirebaseAnlytics = FirebaseAnalytics.getInstance(this);
+        mFireclick = (Button) findViewById(R.id.fire);
+
+        mFireclick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Firebase logevent
+                Bundle bundle = new Bundle();
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID,"btn_click");
+                bundle.putString(FirebaseAnalytics.Param.ITEM_NAME,"Next Activty");
+                bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE,"Button");
+                mFirebaseAnlytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT,bundle);
+
+            }
+        });
+
+    }
+}
